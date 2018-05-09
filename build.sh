fdcf863f1e3274bb2eb4e9b11cb0417862a358dd
root <root@mayun.gitlab.local>
2018-05-09 05:25:14 -0400

    add curl plug

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ sudo apt update
 
 sudo apt install vim git
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 cp .vimrc ~/
 
 cp .vimrc.bundles ~/
